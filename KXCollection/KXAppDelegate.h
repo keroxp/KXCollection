415435767610cbcfa2dc60d411e355a7e847bcc6
Yusuke Sakurai <kerokerokerop@gmail.com>
2014-02-14 17:24:19 +0900

    Initial Commit

---
+++ KXCollection/KXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KXAppDelegate.h
+//  KXCollection
+//
+//  Created by 桜井雄介 on 2014/02/14.
+//  Copyright (c) 2014年 Yusuke Sakurai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
